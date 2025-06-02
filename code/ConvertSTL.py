b301c246fcb331595dd422dd5dd5c020824b14a7
Austin Baird <ajbaird86@gmail.com>
2025-06-02 12:14:04 -0700

    abaird-helper functions and a converter file, tested on current cric and it seems to work

---
+++ code/ConvertSTL.py
@@ -0,0 +1,61 @@
+import sys
+from lib3mf_common import *
+
+
+def find_extension(filename):
+    idx = filename.rfind('.')
+    if idx != -1:
+        return filename[idx:]
+    return ""
+
+
+def convert(filename):
+    # Get a wrapper object
+    wrapper = get_wrapper()
+
+    # Check version always
+    get_version(wrapper)
+
+    extension = find_extension(filename).lower()
+    reader_name, writer_name, new_extension = "", "", ""
+
+    if extension == ".stl":
+        reader_name = "stl"
+        writer_name = "3mf"
+        new_extension = ".3mf"
+    elif extension == ".3mf":
+        reader_name = "3mf"
+        writer_name = "stl"
+        new_extension = ".stl"
+
+    if not reader_name:
+        print(f"Unknown input file extension: {extension}")
+        return -1
+
+    output_filename = filename[:-len(extension)] + new_extension
+
+    model = wrapper.CreateModel()
+    reader = model.QueryReader(reader_name)
+    print(f"Reading {filename}...")
+    reader.ReadFromFile(filename)
+
+    writer = model.QueryWriter(writer_name)
+    print(f"Writing {output_filename}...")
+    writer.WriteToFile(output_filename)
+    print("Done")
+    return 0
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage:")
+        print("Convert 3MF to STL: python3 ConvertSTL.py model.3mf")
+        print("Convert STL to 3MF: python3 ConvertSTL.py model.stl")
+    else:
+        try:
+            result = convert(sys.argv[1])
+            sys.exit(result)
+        except Exception as e:
+            print(str(e)+ "\nAn error occurred during conversion.")
+            print("Please ensure you have the correct file format and the lib3mf library is properly installed.")
+            sys.exit(1)
